43b87d56faae07b3cd1434c1754202b09b4e7eb6
Saifulloi <skyline89_88@mail.ru>
2021-11-05 16:24:27 +0300

    hw05_003

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
 
-from ..models import Group, Post, Follow
+from ..models import Group, Post
 
 User = get_user_model()
 
