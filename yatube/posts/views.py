2c3c2ba3961e2183b5331364224c5ceae5345ae7
Saifulloi <skyline89_88@mail.ru>
2021-11-07 03:33:12 +0300

    hw05_004

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import PostForm, CommentForm
-from .models import Group, Post, User, Comment, Follow
+from .models import Group, Post, User, Follow
 
 
 def index(request):
